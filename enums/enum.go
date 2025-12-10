21feb0e718c2bce90cd6252fa90c074369218eaa
Tejprakash18 <tej22upa.dhyay@gmail.com>
2025-12-10 20:08:40 +0530

    enums

---
+++ enums/enum.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// type OrderStatus intx
+type OrderStatus string
+
+const (
+	// Received OrderStatus = iota
+	Received OrderStatus = "received"
+	Confirmed = "confirmed"
+	Prepared = "prepared"
+	Delivered = "delivered"
+)
+
+func changeOrderStatus(status OrderStatus) {
+	fmt.Println("Order Status ", status)
+}
+func main() {
+
+	changeOrderStatus(Received)
+}
